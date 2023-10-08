a759fc0bc5c26ff941eeb6090a4071dccfb5f850
Norman Chen <113641259+normanchenn@users.noreply.github.com>
2023-10-08 00:35:14 -0400

    trying to get serverless to work

--- app/express-app/index.js
+++ app/express-app/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+const dotenv = require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
