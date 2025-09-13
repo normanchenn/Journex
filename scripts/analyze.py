b71ec5347a4ab66b0e2c68053356fe4c68eb3aee
Norman Chen <njchen@uwaterloo.ca>
2025-09-13 16:34:07 -0400

    add low risk file filtering

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -5,6 +5,9 @@ import requests
 from dataclasses import dataclass
 from typing import Optional
 
+LOW_RISK_DIRECTORIES = ["docs", "documentation", "test", "example"]
+LOW_RISK_EXTENSIONS = ["md", "csv", "txt", "yml", "yaml", "json", "lock"]
+
 @dataclass
 class PRFile:
     sha: str
@@ -17,6 +20,24 @@ class PRFile:
     raw_url: str
     contents_url: str
     patch: Optional[str] = None
+
+def is_low_risk_file(prfile: PRFile) -> bool:
+    filename = prfile.filename.lower()
+
+    for d in LOW_RISK_DIRECTORIES:
+        if d in filename:
+            return True
+    
+    _, ext = os.path.splitext(fname)
+    if ext and ext.startswith("."):
+        ext = ext[1:]
+    if ext in LOW_RISK_EXTENSIONS:
+        return True
+
+    return False
+
+def filter_low_risk(prfiles: list[PRFile]) -> list[PRFile]:
+    return [f for f in prfiles if is_low_risk_file(f)]
 
 def load_pr_files(pr_json_data: str) -> list[PRFile]:
     return [PRFile(**item) for item in pr_json_data]
@@ -50,6 +71,12 @@ def main():
     prDiff = get_pr_files(githubToken, args.repo, args.pr)
     prFiles = load_pr_files(prDiff)
     print(prFiles)
+
+    filteredPRFiles = filter_low_risk(prFiles)
+    print(filteredPRFiles)
+
+    print(len(prFiles))
+    print(len(filteredPRFiles))
 
     body = f"Testing 1"
     with open(githubOutput, "a") as f:
